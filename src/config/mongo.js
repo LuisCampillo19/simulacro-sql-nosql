bb36a7662b5f2d5cd77e257710e53ba949a44a81
LuisCampillo19 <lgcampillo19@gmail.com>
2026-03-01 18:56:08 -0500

    fix: Solution for node version 24+, give it Google's DNS

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,3 +1,7 @@
+// Solution for node version 24+, give it Google's DNS
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import mongoose from "mongoose";
 import 'dotenv/config';
 
